32f860edb19a4a1ffaa74410d62d5d35bd67f55b
Frozen <frozen@me.com>
2026-01-29 23:30:40 +0530

    the commit made to finally run the cart logic

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
 
   return (
     <CartProvider>
-      {cartIsShown && <div>Cart...</div>}
-      <Cart />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
